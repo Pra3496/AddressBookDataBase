84ead2838fbd310ae0a7d273d28677f9742e9c87
Pranav Waghmare <piyushw55@gmail.com>
2023-03-22 17:25:10 +0530

    [Pranav]Added: Ability to Insert Data

--- AddressBookDataBase/AddressBookDataBase.sql
+++ AddressBookDataBase/AddressBookDataBase.sql
@@ -19,4 +19,23 @@ PhoneNumber BIGINT NOT NULL,
 Email VARCHAR(50) NOT NULL
 );
 
+SELECT * FROM AddressBook_DB
+DROP TABLE AddressBook_DB
 
+-- UC3 Inserting new Contact 
+INSERT INTO AddressBook_DB(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
+VALUES
+('Pranav','Waghmare','wardha','Nagpur','Maharastra',442001,963214587,'piyushw55@gmail.com'),
+('Tony','Stark','Stark Tower','ManHatton','New York',442221,963214587,'piyushw55@gmail.com'),
+('Steve','Rogers','Broklyen','New York','NewYork',412001,9632148647,'piyushw55@gmail.com'),
+('Thor','Odinson','Byfrost Asgard','Asgard Mantion','Nine relem',447001,9634214587,'piyushw55@gmail.com')
+
+--UC3 Update Data
+
+UPDATE AddressBook_DB SET LastName = 'Waghmares' WHERE FirstName = 'Pranav'
+UPDATE AddressBook_DB SET Address = 'Pune' WHERE FirstName = 'Pranav'
+UPDATE AddressBook_DB SET City = 'nanded' WHERE FirstName = 'Pranav'
+UPDATE AddressBook_DB SET State = 'Washinton' WHERE FirstName = 'Pranav'
+UPDATE AddressBook_DB SET Zip = 159753 WHERE FirstName = 'Pranav'
+UPDATE AddressBook_DB SET PhoneNumber = 9994447775 WHERE FirstName = 'Thor'
+UPDATE AddressBook_DB SET Email = 'RogersS@Starkins.com' WHERE FirstName = 'Steve'
